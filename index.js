8794bed9504146024cd79102752867655e6637d9
Filippo-Angaroni <96851393+Filippo-Angaroni@users.noreply.github.com>
2025-10-25 02:30:04 +0200

    Update

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+function acceptOnlyNums(e) {
+    //e.target.value = e.target.value.replace("/[^0-9]", '');
+    console.log(e);
+    if (e.data.replace("/[^0-9]", '') == '') {
+        e.target.value.slice(0, -1);
+    }
+}
+
+document.getElementById("w-foglio-macchina").addEventListener("input", acceptOnlyNums);
+
+
+
+
 function imposition(formato_f, formato_m){
     alert(formato_f);
     return 0;
