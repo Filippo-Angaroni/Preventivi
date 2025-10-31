4d13d156b30d00b886d40f10721f8c5c9b4f0a89
Filippo-Angaroni <96851393+Filippo-Angaroni@users.noreply.github.com>
2025-10-31 20:19:06 +0100

    Update

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+disposizioniPagineAffiancate = [1, 2, 4, 8, 16]
+
+class Preventivo {
+
+    constructor(name, paginaSingola, dimensioniMacchima, formatoMacchina, formatoFinito, numPagine) {
+        this.name = name;
+        this.paginaSingola = paginaSingola;
+        this.dimensioniMacchima = dimensioniMacchima;
+        this.formatoMacchina = formatoMacchina;
+        this.formatoFinito = formatoFinito;
+        this.numPagine = numPagine;
+
+        this.disposizioneFogli = this.imposition();
+
+        this.fogliMacchinaNecessari = this.numPagine / (this.disposizioneFogli[0] * this.disposizioneFogli[1] * 2);
+
+        console.log(this.fogliMacchinaNecessari);
+
+    }
+
+    imposition() {
+        if (this.paginaSingola) {
+
+        } else {
+
+            let pagineXX = Math.floor((this.formatoMacchina[0] - 2) / (this.formatoFinito[0] * 2 + 1));
+            let pagineYY = Math.floor((this.formatoMacchina[1] - 2.5 - 1) / (this.formatoFinito[1] + 1));
+            let pagineXY = Math.floor((this.formatoMacchina[0] - 2) / (this.formatoFinito[1] + 1));
+            let pagineYX = Math.floor((this.formatoMacchina[1] - 2.5 - 1) / (this.formatoFinito[0] * 2 + 1));
+
+             
+            if (disposizioniPagineAffiancate.includes(pagineXX)) {
+                if (pagineXX * pagineYY > pagineXY * pagineYX) {
+                    console.log(pagineXX, pagineYY, pagineXY, pagineYX);
+                    return [pagineXX * 2, pagineYY];
+                }
+            }
+
+            if (disposizioniPagineAffiancate.includes(pagineYX)) {
+                console.log(pagineXX, pagineYY, pagineXY, pagineYX);
+                return [pagineYX * 2, pagineXY];
+            } 
+
+            alert("error");
+
+            return 0;
+
+        }
+    }
+}
+
 function acceptOnlyNums(e) {
 
     e.target.value = e.target.value.replace(/[^0-9.,]/, "")
@@ -9,21 +60,18 @@ document.getElementById("h-foglio-macchina").addEventListener("input", acceptOnl
 document.getElementById("w-formato-finito").addEventListener("input", acceptOnlyNums);
 document.getElementById("h-formato-finito").addEventListener("input", acceptOnlyNums);
 
-
-
-
-function imposition(){
+function solve(){
     
-    let fmw = parseFloat(document.getElementById("w-foglio-macchina").value);
-    let fmh = parseFloat(document.getElementById("h-foglio-macchina").value);
-    let ffw = parseFloat(document.getElementById("w-formato-finito").value);
-    let ffh = parseFloat(document.getElementById("h-formato-finito").value);
+    let formatoMacchina = [parseFloat(document.getElementById("w-foglio-macchina").value), parseFloat(document.getElementById("h-foglio-macchina").value)];
+    let formatoFinito = [parseFloat(document.getElementById("w-formato-finito").value), parseFloat(document.getElementById("h-formato-finito").value)];
+
+    let numPagine = parseFloat(document.getElementById("numero-di-pagine").value);
+
+    const preventivo = new Preventivo("test", false, (0, 0), formatoMacchina, formatoFinito, numPagine);
 
     //Se lato x su lato x è meglio di lato x su lato y
     // (Larghezza formato macchina - 2) / (larghezza foglio macchina * 2 - 1) * ((altezza fromato macchina - 2.5) / (altezza formato finito - 1))
-    if (Math.floor((fmw - 2) / (ffw * 2 - 1)) * Math.floor((fmh - 2.5) / (ffh - 1)) > Math.floor((fmw - 2) / (ffh - 1)) * Math.floor((fmh - 2.5) / (ffw * 2 - 1))) {
-        console.log(Math.floor((fmw - 2) / (ffw * 2 - 1)), Math.floor((fmh - 2.5) / (ffh - 1)), Math.floor((fmw - 2) / (ffh - 1)), Math.floor((fmh - 2.5) / (ffw * 2 - 1)))
-    }
+    
 
     return 0;
 }
